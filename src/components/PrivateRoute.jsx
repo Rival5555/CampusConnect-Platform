820d5b0cac13a2a2920c2d803f1da43e03b1b2e1
Hassan Ali <dilruba93r@gmail.com>
2025-12-24 02:35:41 +0500

    adding private route for admin

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,20 +1,9 @@
 import { Navigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../firebase';
 import { Loader2 } from 'lucide-react';
+import { useAuth } from '../context/AuthContext';
 
 export default function PrivateRoute({ children }) {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-            setLoading(false);
-        });
-        return () => unsubscribe();
-    }, []);
+    const { currentUser, loading } = useAuth();
 
     if (loading) {
         return (
@@ -24,5 +13,5 @@ export default function PrivateRoute({ children }) {
         );
     }
 
-    return user ? children : <Navigate to="/login" />;
+    return currentUser ? children : <Navigate to="/login" />;
 }
