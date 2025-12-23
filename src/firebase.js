d672a3c9e77d5c8be6fbd7646784c67ee25bba2c
Hassan Ali <dilruba93r@gmail.com>
2025-12-24 03:17:33 +0500

    adding firebase emulators

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,34 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyAGQOUf79d8aH_uDnR6hMMXTDqK-Q2JYKQ",
+    authDomain: "unievent-84e46.firebaseapp.com",
+    projectId: "unievent-84e46",
+    storageBucket: "unievent-84e46.firebasestorage.app",
+    messagingSenderId: "373352233712",
+    appId: "1:373352233712:web:96497e90d9680a89526035"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Connect to emulators in development
+if (location.hostname === "localhost") {
+    console.log("Using Firebase Emulators");
+    const { connectAuthEmulator } = await import("firebase/auth");
+    const { connectFirestoreEmulator } = await import("firebase/firestore");
+
+    connectAuthEmulator(auth, "http://localhost:9099");
+    connectFirestoreEmulator(db, "localhost", 8080);
+}
+
 export default app;
